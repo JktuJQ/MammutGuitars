12af03269e8c79d3c2c745f0ae024d9cd58fc752
JktuJQ <odmamontov@gmail.com>
2024-07-12 22:45:33 +0300

    Implemented 'features' section
    Implemented 'features' section that contains 3 featured guitars and short descriptions of them.

---
+++ backend/guitar_parsing.py
@@ -0,0 +1,35 @@
+# Imports
+from data.db_sesions import main_session
+from data.models import *
+
+
+def parse_guitar(guitar_id: int) -> dict:
+    """Gathers all data about guitar that matches given `guitar_id`."""
+    result = dict()
+
+    guitar = main_session.query(Guitar).filter(Guitar.id == guitar_id).first()
+    result["id"] = guitar.id
+    result["title"] = f"Guitar №{guitar.id}"
+    result["years"] = guitar.years
+    result["info"] = guitar.info
+    result["stats"] = {
+        "Guitar type": main_session.query(GuitarType).filter(GuitarType.id == guitar.guitar_type_id).first().type,
+        "Wood type": main_session.query(WoodType).filter(WoodType.id == guitar.wood_type_id).first().type,
+        "Body type": main_session.query(BodyType).filter(BodyType.id == guitar.body_type_id).first().type,
+        "Bridge type": main_session.query(BridgeType).filter(BridgeType.id == guitar.bridge_type_id).first().type,
+        "Pickups": [
+            main_session.query(PickupType).filter(PickupType.id == pickup.pickup_id).first().type
+            for pickup in main_session.query(Pickups).filter(Pickups.guitar_id == guitar.id).order_by(Pickups.id).all()
+        ]
+    }
+
+    images_folder = "assets/guitars/"
+    images = list(map(
+        lambda x: images_folder + x.image_filename,
+        main_session.query(Image).filter(Image.guitar_id == guitar_id).order_by(Image.is_icon.desc()).all()
+    ))
+
+    result["icon"] = images[0]
+    result["images"] = images
+
+    return result
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,6 @@
 # Imports
 from backend.application import application
+from backend.guitar_parsing import parse_guitar
 
 from flask import render_template
 import pytube
@@ -14,5 +15,6 @@ def index():
     channel = pytube.Channel("https://www.youtube.com/c/ProgrammingKnowledge")
     videos, links = channel.videos[:3], channel.video_urls[:3]
     data["recent"] = [{"title": video.title, "link": link} for video, link in zip(videos, links)]
+    data["featured"] = [parse_guitar(12), parse_guitar(13), parse_guitar(18)]
 
     return render_template("index.html", data=data)
