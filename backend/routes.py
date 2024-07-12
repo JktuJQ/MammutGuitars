190f6ada197ec5e73e4e30b3b517bcbd323fe012
JktuJQ <odmamontov@gmail.com>
2024-07-12 15:03:44 +0300

    Implemented 'recent videos' section
    Implemented 'recent videos' section using `pytube` and `flask` templating. Currently it's not my channel that is displayed - planning to change that for release.

--- backend/routes.py
+++ backend/routes.py
@@ -2,9 +2,17 @@
 from backend.application import application
 
 from flask import render_template
+import pytube
 
 
 @application.route('/', methods=["GET"])
 def index():
     """Website index page"""
-    return render_template("index.html")
+
+    data = dict()
+    # channel = pytube.Channel("https://www.youtube.com/channel/UCNW8LgJ3hAJvfXnWZWKjrMQ")
+    channel = pytube.Channel("https://www.youtube.com/c/ProgrammingKnowledge")
+    videos, links = channel.videos[:3], channel.video_urls[:3]
+    data["recent"] = [{"title": video.title, "link": link} for video, link in zip(videos, links)]
+
+    return render_template("index.html", data=data)
